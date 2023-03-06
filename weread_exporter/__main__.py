dd647c84ef66ee2b662aea7fc876d14b31f04a8f
drunkdream <drunkdream@qq.com>
2023-03-06 23:33:09 +0800

    Add output format args

--- weread_exporter/__main__.py
+++ weread_exporter/__main__.py
@@ -11,7 +11,15 @@ async def main():
         prog="weread-exporter", description="WeRead book export cmdline tool"
     )
     parser.add_argument("-b", "--book-id", help="book id", required=True)
+    parser.add_argument(
+        "-o",
+        "--output-format",
+        help="output file format",
+        action="append",
+        choices=["md", "epub", "pdf"],
+    )
     args = parser.parse_args()
+    args.output_format = args.output_format or ["epub", "pdf"]
 
     page = webpage.WeReadWebPage(
         args.book_id, cookie_path=os.path.join("cache", "cookie.txt")
@@ -21,7 +29,17 @@ async def main():
     exporter = export.WeReadExporter(page, save_path)
     await exporter.export_markdown()
     await exporter.pre_process_markdown()
-    await exporter.markdown_to_epub("%s.epub" % args.book_id)
+    title = await exporter.get_book_title()
+    if "epub" in args.output_format:
+        save_path = "%s.epub" % title
+        await exporter.markdown_to_epub(save_path)
+        logging.info("Save file %s complete" % save_path)
+
+    if "pdf" in args.output_format:
+        for font_size, desc in ((14, "small"), (32, "large")):
+            save_path = "%s-%s.pdf" % (title, desc)
+            await exporter.markdown_to_pdf(save_path, font_size=font_size)
+            logging.info("Save file %s complete" % save_path)
 
 
 if __name__ == "__main__":
