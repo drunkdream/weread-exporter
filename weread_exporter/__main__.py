c26c649ee34b9c18afba4f75e5e116c2c4ca965b
drunkdream <drunkdream@qq.com>
2024-11-02 19:49:20 +0800

    Add args

--- weread_exporter/__main__.py
+++ weread_exporter/__main__.py
@@ -31,13 +31,13 @@ async def async_main():
         "--load-timeout",
         help="load chapter page timeout",
         type=int,
-        default=30,
+        default=60,
     )
     parser.add_argument(
         "--load-interval",
         help="load chapter page interval time",
         type=int,
-        default=10,
+        default=30,
     )
     parser.add_argument(
         "--css-file",
@@ -48,6 +48,18 @@ async def async_main():
     )
     parser.add_argument(
         "--force-login", help="force login first", action="store_true", default=False
+    )
+    parser.add_argument(
+        "--use-default-profile",
+        help="use default profile",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--mock-user-agent",
+        help="use mock user-agent",
+        action="store_true",
+        default=False,
     )
     args = parser.parse_args()
     args.output_format = args.output_format or ["epub"]
@@ -70,7 +82,9 @@ async def async_main():
     for book_id in book_list:
         logging.info("Exporting book %s" % book_id)
         page = webpage.WeReadWebPage(
-            book_id, cookie_path=os.path.join("cache", "cookie.txt")
+            book_id,
+            cookie_path=os.path.join("cache", "cookie.txt"),
+            webcache_path="cache"
         )
         if not await page.check_valid():
             logging.warning("Book %s status is invalid, stop exporting" % book_id)
@@ -82,10 +96,15 @@ async def async_main():
         exporter = export.WeReadExporter(page, save_path)
         while True:
             try:
-                await page.launch(headless=args.headless, force_login=args.force_login)
+                await page.launch(
+                    headless=args.headless,
+                    force_login=args.force_login,
+                    use_default_profile=args.use_default_profile,
+                    mock_user_agent=args.mock_user_agent,
+                )
             except RuntimeError:
                 logging.exception("Launch book %s home page failed" % book_id)
-                time.sleep(2)
+                await asyncio.sleep(2)
                 continue
 
             try:
@@ -151,7 +170,13 @@ def main():
     logging.root.addHandler(handler)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(async_main())
+    try:
+        loop.run_until_complete(async_main())
+    except:
+        import traceback
+
+        traceback.print_exc()
+        return
 
 
 if __name__ == "__main__":
